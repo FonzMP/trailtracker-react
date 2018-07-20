7dd6f8c472f010f9f51e96d29725d09d7ff44969
fonzmp <fonzmp@gmail.com[color]>
2018-07-20 15:35:51 -0400

    pulled user views and controller info from old project

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,51 @@
+class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.create(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      
+      redirect_to user_path(@user)
+    else
+      render "new"
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.update(user_params)
+    if @user
+      redirect_to user_path(@user)
+    else
+      redirect_to trails_path
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id]).destroy
+
+    redirect_to trails_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+
+end
