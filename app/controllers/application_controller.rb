ff8e2b00c29347d838c2db353d386d48c645db1a
fonzmp <fonzmp@gmail.com[color]>
2018-07-22 18:16:06 -0400

    added current_user to application controller and nav partial

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find(session[:user_id])
+    else
+      redirect_to login_path
+    end
+  end
+  
 end
