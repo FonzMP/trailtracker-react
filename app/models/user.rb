245231162c0ca6a9f9464271c2eeb8e33613c3f8
fonzmp <fonzmp@gmail.com[color]>
2018-07-22 14:08:54 -0400

    created unowned trails helper

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,8 @@ class User < ApplicationRecord
   has_many :trail_ratings
   has_many :trails, through: :trail_ratings
 
+  def unowned_trails
+    (Trail.all.map{|t| t.id} - self.trail_ids).map{|id| Trail.find(id)}
+  end
+
 end
