22f49e761127b40bcec76b13872c929b1909364f
fonzmp <fonzmp@gmail.com[color]>
2018-07-22 13:26:13 -0400

    logic for trail_rating creation

--- app/controllers/trail_ratings_controller.rb
+++ app/controllers/trail_ratings_controller.rb
@@ -1,6 +1,15 @@
 class TrailRatingsController < ApplicationController
 
   def create
-    binding.pry
+    @trail_rating = TrailRating.create(trail_rating_params)
+    if @trail
+      redirect_to user_path(@trail_rating.user)
+    end
+  end
+
+  private
+
+  def trail_rating_params
+    params.require(:trail_rating).permit(:user_id, :trail_id, :rating)
   end
 end
