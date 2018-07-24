2c266a3b017fa3eb95ac93311a1328c59ec67796
fonzmp <fonzmp@gmail.com[color]>
2018-07-24 00:27:01 -0400

    called current_user before welcome to avoid @current_user error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,14 @@
 class ApplicationController < ActionController::Base
 
+  before_action :current_user
+
   def welcome
-    
+
   end
 
   def current_user
     if session[:user_id]
       @current_user ||= User.find(session[:user_id])
-    else
-      redirect_to login_path
     end
   end
 
