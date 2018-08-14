c73fb1db019311fe01e9275b454bbb88497053ce
fonzmp <fonzmp@gmail.com[color]>
2018-08-14 15:57:36 -0400

    relocated button js to buttons.js

--- app/assets/javascripts/buttons.js
+++ app/assets/javascripts/buttons.js
@@ -1,3 +1,66 @@
+$(function() {
+  $("#trails").click(function(e) {
+    $.get("/trails.json", function(data) {
+      $("#all_trails").html(" ");
+      data.forEach(function(trail) {
+        $("#all_trails").append(
+          `
+          <p><strong>Name: </strong>${trail.name}</p>
+          <div id="trail-info-${trail.id}">
+          <a href="#" id="trail-${trail.id}" data-trail="${
+            trail.id
+          }" class="trail-details" >View ${trail.name} Trail Info</a>
+          </div>
+          <br>
+          `
+        );
+      });
+      trailDetails();
+    });
+
+    if ($(this).text() === "View All Trails") {
+      $(this).text("Hide All Trails");
+      $("#all_trails").removeClass("hidden");
+    } else {
+      $(this).text("View All Trails");
+      $("#all_trails").addClass("hidden");
+    }
+  });
+
+  toggleDisplay(
+    "#user-trails",
+    "View Trails You've Created",
+    "Hide Your Trails",
+    "#user_created_trails"
+  );
+
+  toggleDisplay(
+    "#user-trail-ratings",
+    "View Your Trail Ratings",
+    "Hide Your Trail Ratings",
+    "#user_tr_display"
+  );
+
+  toggleDisplay(
+    "#add-trail",
+    "Add a New Trail",
+    "Hide New Trail Form",
+    "#trail_form"
+  );
+
+  toggleDisplay(
+    "#add-trail-rating",
+    "Add a Trail Rating",
+    "Hide Trail Rating Form",
+    "#trail_ratings_form"
+  );
+
+  $("a.trail-details").click(function(e) {
+    console.log(this);
+    e.preventDefault();
+  });
+});
+
 function toggleDisplay(buttonClicked, showText, hideText, renderedForm) {
   $(buttonClicked).click(function(e) {
     if ($(this).text() === showText) {
