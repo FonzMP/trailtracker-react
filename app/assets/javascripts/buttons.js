d57951ba3cccc0bd14de33fd31ead6119e129a30
fonzmp <fonzmp@gmail.com[color]>
2018-08-14 17:15:51 -0400

    relocated js logic to buttons.js

--- app/assets/javascripts/buttons.js
+++ app/assets/javascripts/buttons.js
@@ -1,4 +1,43 @@
-$(function() {
+function toggleDisplay(buttonClicked, showText, hideText, renderedForm) {
+  $(buttonClicked).click(function(e) {
+    if ($(this).text() === showText) {
+      $(this).text(hideText);
+      $(renderedForm).removeClass("hidden");
+    } else {
+      $(this).text(showText);
+      $(renderedForm).addClass("hidden");
+    }
+  });
+}
+
+function trailDetails() {
+  $("a.trail-details").click(function(e) {
+    e.preventDefault();
+    const trailId = $(this).data("trail");
+    $.get(`/trails/${trailId}.json`, function(data) {
+      const trailName = data.name;
+      $(`#trail-info-${trailId}`).html(
+        `
+        <p><strong>Length: </strong> ${data.length}</p>
+        <a href="#" id="hide-info-${trailId}">Hide Trail Info</a>
+      `
+      );
+      hideDetails(trailId, trailName);
+    });
+  });
+}
+
+function hideDetails(id, name) {
+  $("#hide-info-" + id).click(function(e) {
+    e.preventDefault();
+    $("#trail-info-" + id).html(
+      `<a href="#" id="trail-${id}" data-trail="${id}" class="trail-details" >View ${name} Trail Info</a>`
+    );
+    trailDetails();
+  });
+}
+
+function attachListeners() {
   $("#trails").click(function(e) {
     $.get("/trails.json", function(data) {
       $("#all_trails").html(" ");
@@ -54,48 +93,4 @@ $(function() {
     "Hide Trail Rating Form",
     "#trail_ratings_form"
   );
-
-  $("a.trail-details").click(function(e) {
-    console.log(this);
-    e.preventDefault();
-  });
-});
-
-function toggleDisplay(buttonClicked, showText, hideText, renderedForm) {
-  $(buttonClicked).click(function(e) {
-    if ($(this).text() === showText) {
-      $(this).text(hideText);
-      $(renderedForm).removeClass("hidden");
-    } else {
-      $(this).text(showText);
-      $(renderedForm).addClass("hidden");
-    }
-  });
-}
-
-function trailDetails() {
-  $("a.trail-details").click(function(e) {
-    e.preventDefault();
-    const trailId = $(this).data("trail");
-    $.get(`/trails/${trailId}.json`, function(data) {
-      const trailName = data.name;
-      $(`#trail-info-${trailId}`).html(
-        `
-        <p><strong>Length: </strong> ${data.length}</p>
-        <a href="#" id="hide-info-${trailId}">Hide Trail Info</a>
-      `
-      );
-      hideDetails(trailId, trailName);
-    });
-  });
-}
-
-function hideDetails(id, name) {
-  $("#hide-info-" + id).click(function(e) {
-    e.preventDefault();
-    $("#trail-info-" + id).html(
-      `<a href="#" id="trail-${id}" data-trail="${id}" class="trail-details" >View ${name} Trail Info</a>`
-    );
-    trailDetails();
-  });
 }
