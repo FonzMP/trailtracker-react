0ee05de187ff21211fbd491c9b1a02704a289c6d
fonzmp <fonzmp@gmail.com[color]>
2018-08-16 00:45:27 -0400

    opened index for trail_ratings and rated_trails to pull from

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,8 @@ Rails.application.routes.draw do
     resources :trails, only: [:new, :create, :index]
   end
   resources :trails
-  resources :trail_ratings, only: [:show, :create, :edit, :destroy, :update]
+  resources :trail_ratings, only: [:index, :show, :create, :edit, :destroy, :update]
+  get '/trails_rated', to: "trail_ratings#rated_trails"
   get '/signup', to: "users#new"
   get '/login', to: "sessions#new"
   post '/login', to: "sessions#create"
